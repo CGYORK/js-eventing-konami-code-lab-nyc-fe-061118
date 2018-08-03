199941a9a079f5f89e4cc2295797263e9c554af2
Christian Georgeou <cgeorgeou@hotmail.com>
2018-08-03 17:38:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 const body = document.getElementById("CheatCode");
 
-body.addEventListener('keydown', init)
+$("#CheatCode").on('keydown', init)
+body.addEventListern('keydown', init)
 
 function init(e) {
   // Write your JavaScript code inside the init() function
@@ -11,7 +12,6 @@ function init(e) {
       index++;
       if (index === code.length){
         alert("Don't cheat! Play the game like a man.");
-        index = 0;
       }
     } else {
       index = 0; 
